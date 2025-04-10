1f32761424adf1225fe40c1811050e00817b2d43
txger. <simaocampanico.dev@gmail.com>
2025-04-10 15:13:47 +0100

    Editing MilitaryUnit.cs with the Health, Cost and the Attack method.

--- GameUnit/MilitaryUnit.cs
+++ GameUnit/MilitaryUnit.cs
@@ -1,14 +1,28 @@
 namespace GameUnit
 {
-    public class MilitaryUnit : Unit // Heranca!!
+    public class MilitaryUnit : Unit
     {
         public int AttackPower { get; }
         public int XP { get; private set; }
 
         public MilitaryUnit(int mov, int health, int attackPower) : base(mov, health)
-        {                                                        // Unit(int, int)
+        {
             AttackPower = attackPower;
             XP = 0;
+        }
+
+        public override int Health
+        {
+            get => base.Health + XP;
+            set => base.Health = value;
+        }
+
+        public override float Cost => AttackPower + XP;
+
+        public void Attack(Unit u)
+        {
+            XP++;
+            u.Health -= AttackPower;
         }
     }
 }
